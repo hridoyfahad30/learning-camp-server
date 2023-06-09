b27a479a8c3b1539028d3ffcb7a4e3511316dc48
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-06-09 13:53:45 +0600

    Create API for Store User

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200
+}
 
 
 // middleWare
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
@@ -32,17 +39,18 @@ async function run() {
     const usersCollection = client.db("learningCamp").collection("allUsers");
 
 
-
-    app.get('/allUsers', async (req, res) => {
-        const result = await usersCollection.find().toArray();
-        res.send(result)
+    app.put('/allUsers/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = {email: email};
+      const options = {upsert: true};
+      const updateUser = {$set: user}
+      const result = await usersCollection.updateOne(query, updateUser, options);
+      res.send(result);
     });
 
-    app.post('/allUsers', async (req, res) => {
-        const user = req.body;
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-    });
+   
+    
 
 
 
