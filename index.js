6e5a2da46d3b5f4319a064857af47d86d265e582
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-06-12 23:02:02 +0600

    Final

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express= require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
 const app = express();
+const stripe = require('stripe')(process.env.PAYMENT_SECTRE_KEY)
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -144,6 +145,20 @@ async function run() {
       const query = {_id: new ObjectId(id)};
       const result = await selectClassCollection.deleteOne(query);
       res.send(result);
+    })
+
+    // Create Payment Intent
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+      const {price} = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
 
     // Get Instructor Classes by Email
